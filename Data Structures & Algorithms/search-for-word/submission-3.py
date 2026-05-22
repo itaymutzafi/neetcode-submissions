603de222a9bd6db275f879c6a5e325f9e1dce3f9
Itay's Space <itay680@gmail.com>
2026-05-22 19:04:35 +0300

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+        rows = len(board)
+        cols = len(board[0])
+        comp_word = [ch for ch in word]
+        
+
+        def helper(s, i, j):
+            if i >= len(board) or i < 0 or j >= len(board[0]) or j < 0 or board[i][j] is None:
+                return False
+            curr = board[i][j]
+            s.append(curr)
+            board[i][j] = None
+            if s == comp_word:
+                return True
+            
+            for dx, dy in self.dirs:
+                if helper(s, i + dx, j + dy):
+                    return True
+            s.pop()
+            board[i][j] = curr
+            return False
+
+        for i in range(rows):
+            for j in range(cols):
+                tmp = []       
+                if helper(tmp, i, j):
+                    return True
+        return False
+            
