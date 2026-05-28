11af96ebf5598a036a28f430ae53efe4f9b1e8e9
Itay's Space <itay680@gmail.com>
2026-05-28 15:42:51 +0300

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+
+        if not intervals:
+            return res
+
+        intervals.sort(key = lambda x: x[0]) 
+        curr_start, curr_end = intervals[0][0], intervals[0][1]         
+        
+        # [1,3], [1,5], [6,7]
+        # if curr_end <= interval[0], so merge the intervals -> curr_end = interval[1]
+        
+        for i in range(1, len(intervals)):
+            interval = intervals[i]
+            if interval[0] <= curr_end:
+                curr_end = max(curr_end, interval[1])
+            else:
+               res.append([curr_start, curr_end])
+               curr_start, curr_end = interval[0], interval[1]
+        
+        res.append([curr_start, curr_end])
+        return res
+                
+            
