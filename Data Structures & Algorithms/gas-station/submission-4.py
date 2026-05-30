d8b3cce5e2fc98b54f673a71fccb1b4a3c637ed3
Itay's Space <itay680@gmail.com>
2026-05-30 18:53:09 +0300

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # gas = [5,8,2,8], cost = [6,5,6,6] 
+        # [-1, 3, -4, 2]
+        # 0 not possible, total is -1. 1 is possible. 2 is not possible, tot is -1. 3 is possible, total is 2
+
+        total = 0
+        tank = 0
+        res_i = 0
+
+        for i in range(len(gas)):
+            curr = gas[i] - cost[i]
+            total += curr
+            tank += curr
+
+            if tank < 0:
+                tank = 0
+                res_i = i + 1
+        
+        if total >= 0:
+            return res_i
+        else:
+            return -1
+                
+
+        
+                
+                
+                
