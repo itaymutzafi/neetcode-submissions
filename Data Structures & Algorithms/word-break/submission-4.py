998d95a24afd3aaa916582b67816e8e2282b2fba
Itay's Space <itay680@gmail.com>
2026-06-06 13:10:37 +0300

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
+        words.add("")
+        res = s
+        d = {}
+        
+        def helper(i):
+            if i >= len(s):
+                return True
+            if i in d:
+                return d[i]
+            res = False
+            for j in range(i + 1, len(s) + 1):
+                if s[i:j] in words:
+                    res = helper(j) or res
+            d[i] = res
+            return res
+
+        return helper(0)
+
+                    
+                
+            
+        
+        
+        
