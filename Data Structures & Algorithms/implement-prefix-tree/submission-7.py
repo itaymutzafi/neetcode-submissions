0dcf70183f28d019cdf25bb20564538e618486c0
Itay's Space <itay680@gmail.com>
2026-06-07 15:10:40 +0300

    Add: implement-prefix-tree - submission-7

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-7.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, val = "", neighbors = None):
+        self.val = val
+        self.neighbors = {} if not neighbors else neighbors
+        self.isEnd = False
+
+class PrefixTree:
+    def __init__(self):
+        self.root = Node()
+    
+    def insert(self, word: str) -> None:         
+        curr = self.root
+        for i in range(len(word)):
+            tmp = word[i]
+            to_ins = True
+            if tmp not in curr.neighbors:
+                curr.neighbors[tmp] = Node(tmp)
+            curr = curr.neighbors[tmp]
+        curr.isEnd = True
+    
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for i in range(len(word)):
+            tmp = word[i]
+            if tmp in curr.neighbors:
+                curr = curr.neighbors[tmp]
+            else:
+                return False
+        return curr.isEnd
+            
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for i in range(len(prefix)):
+            tmp = prefix[i]
+            if tmp in curr.neighbors:
+                curr = curr.neighbors[tmp]
+            else:
+                return False
+        return True
+        
+        
